53363d45094e54e5039d15c1d9394d5442ab2150
Diego Costa <diego.costa@intelie.com.br>
2021-08-27 10:26:19 -0300

    Implement SuperMnagas.alias

--- src/Hosters/SuperMangas.ts
+++ src/Hosters/SuperMangas.ts
@@ -13,6 +13,25 @@ export default class SuperMangas extends Host {
   get mangaName() {
     return this.host.split("/")[4].trim() || "";
   }
+  get alias() {
+    return [];
+  }
+  get categories() {
+    if (this.isListPage)
+      return Array.from<HTMLLinkElement>(
+        document.querySelectorAll(
+          'ul.boxAnimeSobre>li>span[itemprop="genre"]>a'
+        )
+      ).map((cat) => ({ name: cat.innerText?.toLowerCase(), link: cat.href }));
+    else if (this.isCapPage) {
+      return Array.from<HTMLLinkElement>(
+        document.querySelectorAll("li.sizeFull>span>a.genero_btn")
+      ).map((cat) => ({
+        name: cat.innerText?.toLowerCase(),
+        link: cat.href,
+      }));
+    } else return [];
+  }
 
   get thumb() {
     if (this.isListPage)
