c213debd848ef736fe37c4644afbadc9d36282b5
Diego Costa <diego.costa@intelie.com.br>
2021-08-27 10:26:31 -0300

    updateTypes

--- src/types/MangasTypes.ts
+++ src/types/MangasTypes.ts
@@ -2,8 +2,17 @@ export type MangaType = {
   lastCap: string | number;
   highCap: string | number;
   lastSource: string;
+  lastTime?: number;
   thumb?: string;
   alias?: string[];
+  sources?: { [hostname: string]: string };
+  history?: {
+    [timestamp: number]: string;
+  };
+  categories: {
+    name: string;
+    link?: string;
+  }[];
 };
 
 export type MangaListType = {
