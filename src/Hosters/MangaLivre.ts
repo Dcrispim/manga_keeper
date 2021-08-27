3d81240e159d57aba4d77bb9777041b0ec753295
Diego Costa <diego.costa@intelie.com.br>
2021-08-27 10:25:19 -0300

    implement MangaLivre.alias

--- src/Hosters/MangaLivre.ts
+++ src/Hosters/MangaLivre.ts
@@ -6,6 +6,40 @@ export default class Mangalivre extends Host {
     else return "";
   }
 
+  get alias() {
+    if (this.isListPage)
+      return Array.from<HTMLLinkElement>(
+        document.querySelectorAll("ol.series-synom>li")
+      ).map((li) => li?.innerText?.toLowerCase());
+    else return [] as string[];
+  }
+
+  get lastCap() {
+    if (this.isListPage)
+      return (
+        document?.querySelector(
+          "div.container-box.default.color-brown>h2>span"
+        ) as HTMLSpanElement
+      )?.innerText;
+    else return "";
+  }
+
+  get isDestaq() {
+    return this.host.split("/")[3] === "destaques";
+  }
+
+  get categories() {
+    if (this.isListPage)
+      return Array.from<HTMLLinkElement>(
+        document.querySelectorAll(
+          "div.touchcarousel-wrapper.grab-cursor>ul.tags.touchcarousel-container>li.touchcarousel-item>a>span"
+        )
+      ).map((cat) => ({
+        name: cat.innerText,
+        link: (cat.parentElement as HTMLLinkElement)?.href,
+      }));
+    else return [] as { name: string; link?: string }[];
+  }
   get mangaName() {
     return this.host.split("/")[4].trim() || "";
   }
