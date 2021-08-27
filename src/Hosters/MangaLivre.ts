899270b9b8ce04120be8ecd1b93baa497d90b007
Diego Costa <diego.costa@intelie.com.br>
2021-08-27 10:25:34 -0300

    implement MangaLivre.PageEvent

--- src/Hosters/MangaLivre.ts
+++ src/Hosters/MangaLivre.ts
@@ -1,3 +1,4 @@
+import { MangaListType } from "../types/MangasTypes";
 import Host from "./Host";
 
 export default class Mangalivre extends Host {
@@ -64,5 +65,126 @@ export default class Mangalivre extends Host {
 
   get isListPage() {
     return this.host.split("/").includes("manga");
+  }
+
+  pageEvent() {
+    if (this.isCapPage) {
+      const toolsContainer = document.querySelector<HTMLDivElement>(
+        "div.reader-controls.mobile-controls>div.right-controls"
+      );
+
+      const btn = document.createElement("div");
+      btn.className = "report-container";
+      btn.style = `
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      font-size: x-large;
+      `;
+      btn.innerHTML = `<span style="border-radius:20px" >🔁</span>`;
+      btn.onclick = () =>
+        Array.from<HTMLImageElement>(
+          document.querySelectorAll("div.manga-image>img")
+        ).reduce((prevImgs, img) => {
+          if (prevImgs.includes(img.src)) {
+            console.log(img.src);
+            img.parentElement?.remove();
+          } else {
+            prevImgs.push(img.src);
+          }
+          return prevImgs;
+        }, [] as string[]);
+      toolsContainer?.appendChild(btn);
+
+      //-------------------ZOOM-------------------------------
+      const zoom = document.createElement("div");
+      const zoomBtnPlus = document.createElement("div");
+      const zoomBtnMinus = document.createElement("div");
+
+      zoom.style = `
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      align-items: center;
+      font-size: x-large;
+      color:white;
+      width: 100px;
+      font-weight: bold
+      `;
+
+      zoom.className = "release-rating-container";
+      //zoomBtnMinus.className = "report-container";
+
+      zoomBtnPlus.style =
+        "border-right: 1px solid hsla(0,0%,100%,.25); width:100%";
+      zoomBtnMinus.style = "width:100%";
+
+      zoomBtnPlus.innerHTML = `<span style="border-radius:20px" >+</span>`;
+      zoomBtnMinus.innerHTML = `<span style="border-radius:20px" >-</span>`;
+
+      zoomBtnPlus.onclick = () =>
+        Array.from<HTMLImageElement>(
+          document.querySelectorAll("div.manga-image>img")
+        ).map((img) => {
+          const currenW = img.style.width.includes("%")
+            ? parseFloat(img.style.width)
+            : (parseFloat(img.style.width) / window.innerWidth) * 100;
+
+          return (img.style.width =
+            currenW + 10 <= 100 ? `${currenW + 10}%` : "100%");
+        });
+
+      zoomBtnMinus.onclick = () =>
+        Array.from<HTMLImageElement>(
+          document.querySelectorAll("div.manga-image>img")
+        ).map((img) => {
+          const currenW = parseFloat(img.style.width);
+          return (img.style.width =
+            currenW - 10 >= 10 ? `${currenW - 10}%` : "10%");
+        });
+
+      zoom.appendChild(zoomBtnPlus);
+      zoom.appendChild(zoomBtnMinus);
+
+      toolsContainer?.appendChild(zoom);
+    } else if (this.isDestaq) {
+      const destaqs = Array.from<HTMLDivElement>(
+        document.querySelectorAll("div.destaque-wrap")
+      );
+
+      chrome.storage.local.get(null, (capList: MangaListType) => {
+        destaqs.map((destaq) => {
+          const isRead = document.createElement("div");
+          isRead.innerText = "LIDO";
+          isRead.style = `
+      background-color: rgba(0, 0, 0, 0.6);
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      font-size: 70px;
+      color: rgb(255, 255, 255);
+      font-weight: bold;`;
+          const destaqName = destaq
+            .querySelector<HTMLSpanElement>("a.chapter-link")
+            ?.href.split("/")[4];
+          const destaqCap =
+            destaq.querySelector<HTMLSpanElement>(
+              "a.chapter-link>div.chapter-number-wrap>div.chapter-number>span"
+            )?.innerText || "";
+
+          if (capList[destaqName]?.lastCap?.toString() === destaqCap) {
+
+            destaq.appendChild(isRead);
+          }
+        });
+      });
+    }
   }
 }
